078324495d8c954c9dc21f1a24c2e4ddd3af7b3d
0oAethero0 <ethan.campbell2232@gmail.com>
2026-07-14 22:53:42 -0400

    Create LootInjector.java

---
+++ src/main/java/com/Aether/randomoredrops/LootInjector.java
@@ -0,0 +1,34 @@
+package com.Aether.randomoredrops;
+
+import net.fabricmc.fabric.api.loot.v3.LootTableEvents;
+import net.minecraft.util.Identifier;
+
+public class LootInjector {
+
+    public static void register() {
+
+        LootTableEvents.MODIFY.register((key, tableBuilder, source, registries) -> {
+
+            Identifier id = key.getValue();
+
+            // Only modify block loot tables
+            if (!id.getPath().startsWith("blocks/")) {
+                return;
+            }
+
+            String blockName = id.getPath()
+                    .replace("blocks/", "");
+
+
+            // Temporary test: detect ores
+            if (blockName.contains("ore")) {
+
+                System.out.println(
+                        "Ore loot detected: " + blockName
+                );
+
+            }
+
+        });
+    }
+}
