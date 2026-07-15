44945d1d1607c63e5dd5e757311ecf6b498aa911
0oAethero0 <ethan.campbell2232@gmail.com>
2026-07-14 22:45:26 -0400

    Create RandomOreDrops.java

---
+++ src/main/java/com/0oAethero0/randomoredrops/RandomOreDrops.java
@@ -0,0 +1,15 @@
+package com.0oAethero0.randomoredrops;
+
+import net.fabricmc.api.ModInitializer;
+
+public class RandomOreDrops implements ModInitializer {
+
+    public static final String MOD_ID = "cobblemon_random_ore_drops";
+
+    @Override
+    public void onInitialize() {
+
+        System.out.println("Cobblemon Random Ore Drops loaded!");
+
+    }
+}
