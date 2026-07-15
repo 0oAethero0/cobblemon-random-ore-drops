1abc8b787646f53a1c92561f32fea0903639ba40
0oAethero0 <ethan.campbell2232@gmail.com>
2026-07-14 23:08:23 -0400

    Create LootBuilder.java

---
+++ src/main/java/com/Aether/randomoredrops/LootBuilder.java
@@ -0,0 +1,13 @@
+package com.Aether.randomoredrops;
+
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+
+public class LootBuilder {
+
+    public static ItemStack createDrop(Item item) {
+
+        return new ItemStack(item);
+
+    }
+}
