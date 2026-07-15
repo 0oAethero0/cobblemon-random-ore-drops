8e4c40c0b96768ace2729ab413ff0d139817ffff
0oAethero0 <ethan.campbell2232@gmail.com>
2026-07-14 23:27:15 -0400

    Update Config.java

--- src/main/java/com/Aether/randomoredrops/Config.java
+++ src/main/java/com/Aether/randomoredrops/Config.java
@@ -1,5 +1,8 @@
 package com.Aether.randomoredrops;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Config {
 
     public static boolean enabled = true;
@@ -7,5 +10,8 @@ public class Config {
     public static double normalOreChance = 0.10;
 
     public static double rareOreChance = 0.50;
+
+    public static List<String> blacklist =
+            new ArrayList<>();
 
 }
