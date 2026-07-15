a39cc8a4c9d2426abf8ab136d160460e2b274892
0oAethero0 <ethan.campbell2232@gmail.com>
2026-07-14 23:06:54 -0400

    Update LootInjector.java

--- src/main/java/com/Aether/randomoredrops/LootInjector.java
+++ src/main/java/com/Aether/randomoredrops/LootInjector.java
@@ -1,7 +1,11 @@
 package com.Aether.randomoredrops;
 
 import net.fabricmc.fabric.api.loot.v3.LootTableEvents;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+
+import java.util.Optional;
 
 public class LootInjector {
 
@@ -9,22 +13,38 @@ public class LootInjector {
 
         LootTableEvents.MODIFY.register((key, tableBuilder, source, registries) -> {
 
+
             ResourceLocation id = key.location();
+
 
             if (!id.getPath().startsWith("blocks/")) {
                 return;
             }
 
+
             String blockName = id.getPath()
                     .replace("blocks/", "");
 
-            if (blockName.contains("ore")) {
 
-                System.out.println(
-                        "Ore loot detected: " + blockName
-                );
-
+            if (!blockName.contains("ore")) {
+                return;
             }
+
+
+            Item randomItem =
+                    CobblemonItemPicker.randomItem();
+
+
+            if (randomItem == null) {
+                return;
+            }
+
+
+            System.out.println(
+                    "Would drop: "
+                    + BuiltInRegistries.ITEM
+                    .getKey(randomItem)
+            );
 
         });
     }
