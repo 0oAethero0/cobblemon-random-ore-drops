89f6ea55b5f7947731807bd15471b235ac54f361
0oAethero0 <ethan.campbell2232@gmail.com>
2026-07-14 23:23:34 -0400

    Update JsonConfig.java

--- src/main/java/com/Aether/randomoredrops/JsonConfig.java
+++ src/main/java/com/Aether/randomoredrops/JsonConfig.java
@@ -1,13 +1,106 @@
 package com.Aether.randomoredrops;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+
 public class JsonConfig {
+
+    private static final File FILE =
+            new File("config/cobblemon_random_ore_drops.json");
+
+
+    private static final Gson GSON =
+            new GsonBuilder()
+                    .setPrettyPrinting()
+                    .create();
+
 
     public static void load() {
 
-        System.out.println(
-                "Config loaded"
-        );
+        try {
+
+            if (!FILE.getParentFile().exists()) {
+                FILE.getParentFile().mkdirs();
+            }
+
+
+            if (!FILE.exists()) {
+
+                save();
+
+                return;
+
+            }
+
+
+            FileReader reader =
+                    new FileReader(FILE);
+
+
+            ConfigData data =
+                    GSON.fromJson(reader, ConfigData.class);
+
+
+            reader.close();
+
+
+            Config.enabled =
+                    data.enabled;
+
+            Config.normalOreChance =
+                    data.normalOreChance;
+
+            Config.rareOreChance =
+                    data.rareOreChance;
+
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+        }
 
     }
 
+
+    private static void save() {
+
+        try {
+
+            FileWriter writer =
+                    new FileWriter(FILE);
+
+
+            writer.write(
+                    GSON.toJson(
+                            new ConfigData()
+                    )
+            );
+
+
+            writer.close();
+
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+        }
+
+    }
+
+
+    private static class ConfigData {
+
+        boolean enabled = true;
+
+        double normalOreChance = 0.10;
+
+        double rareOreChance = 0.50;
+
+    }
 }
